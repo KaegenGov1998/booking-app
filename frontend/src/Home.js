01792ec3b5f69e44e4eb231871cd2b7f18432195
KaegenGov1998 <152480939+KaegenGov1998@users.noreply.github.com>
2026-04-07 19:28:54 +0200

    updates for vercel

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,23 +2,27 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const App = () => {
-  const [backendData, setBackendData] = useState([{}]);
+  const [backendData, setBackendData] = useState([]);
 
   useEffect(() => {
     fetch("https://booking-app-brre.onrender.com/bookings")
       .then((response) => response.json())
-      .then((data) => {
-        setBackendData(data);
-      });
+      .then((data) => setBackendData(data))
+      .catch((err) => console.log(err));
   }, []);
 
   const deleteBooking = async (index) => {
     try {
-      const response = await fetch(`https://booking-app-brre.onrender.com/booking/${index}`, {
-        method: "DELETE",
-      });
+      const response = await fetch(
+        `https://booking-app-brre.onrender.com/booking/${index}`,
+        {
+          method: "DELETE",
+        }
+      );
+
       const data = await response.json();
       console.log(data);
+
       window.location.reload();
     } catch (error) {
       console.log("Error", error);
@@ -27,32 +31,28 @@ const App = () => {
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center bg-[#F4F0EA]">
-      <div className="flex flex-col w-[800px] justify-center items-center gap-3 p-5 border-none bg-[#FFFFFF] rounded-lg">
-        <div>
-          <h1 className="text-4xl">Bookings</h1>
-        </div>
+      <div className="flex flex-col w-[800px] justify-center items-center gap-3 p-5 bg-[#FFFFFF] rounded-lg">
+        <h1 className="text-4xl">Bookings</h1>
+
         {backendData.length === 0 ? (
           <p>No Bookings logged</p>
         ) : (
           backendData.map((bookings, i) => (
-            <div className="flex flex-col w-full">
-              <p key={i} className="flex flex-col w-full">
-                <div className="text-xl">{bookings.name}</div>
-                <div className="flex flex-row w-full bg-slate-300 items-stretch">
-                  <div
-                    className="flex flex-col ">
-                    <div className="text-[#611201]">Date</div>
-                <div className="font-semibold">{bookings.date}</div>
+            <div key={i} className="flex flex-col w-full">
+              <div className="text-xl">{bookings.name}</div>
+
+              <div className="flex flex-row w-full bg-slate-300 items-stretch">
+                <div className="flex flex-col">
+                  <div className="text-[#611201]">Date</div>
+                  <div className="font-semibold">{bookings.date}</div>
                 </div>
-                <div className="
-                    flex
-                    flex-col"
-                  >
-                    <div className="text-[#611201]">Time</div>
-                    <div className="font-semibold">{bookings.time}</div>
-                  </div>
+
+                <div className="flex flex-col ml-5">
+                  <div className="text-[#611201]">Time</div>
+                  <div className="font-semibold">{bookings.time}</div>
                 </div>
-              </p>
+              </div>
+
               <div className="flex justify-evenly">
                 <Link
                   to={`update/${i}`}
@@ -60,6 +60,7 @@ const App = () => {
                 >
                   Update
                 </Link>
+
                 <button
                   onClick={() => deleteBooking(i)}
                   className="border-none rounded-lg bg-[#C0573E] p-2 m-2"
@@ -70,6 +71,7 @@ const App = () => {
             </div>
           ))
         )}
+
         <Link to="/booking">Add a booking</Link>
       </div>
     </div>
